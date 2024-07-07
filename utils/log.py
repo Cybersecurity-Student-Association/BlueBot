9ab4a0e50bcffd74f1f80b35dd3db8004a9428b9
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2024-07-07 09:58:46 +0300

    feat: silent log message

--- utils/log.py
+++ utils/log.py
@@ -2,4 +2,4 @@ import discord
 from variables.channels import log_channel
 
 async def log(client: discord.Client, content: str):
-    await client.get_channel(log_channel).send(content=content)
+    await client.get_channel(log_channel).send(content=content, silent=True)
